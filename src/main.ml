f9e73d507654d97d754d8dd24dbbb9d8785076f2
uenoku <uenoku.tokotoko@gmail.com>
2018-10-09 23:12:42 +0900

    fix bug

--- src/main.ml
+++ src/main.ml
@@ -30,9 +30,9 @@ let _ =
     let oname = filename ^ ".txt" in
     let oc = open_out oname in
     let _ = List.iter (Printf.fprintf oc "%s\n") p in
-    Printf.printf "outputed to %s" oname
+    Printf.printf "outputed to %s\n" oname
   else
     let oname = filename ^ ".oo" in
     let oc = open_out oname in
     let _ = output_bytes oc (to_bin p) in
-    Printf.printf "outputed to %s" oname
+    Printf.printf "outputed to %s\n" oname
